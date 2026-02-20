5bfa29722d1195fb796db7ded09244ba53371c6f
Christian <Chris27@email.com>
2026-02-19 22:27:19 -0500

    TEST CONFIG CORS

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,23 +1,19 @@
-<?php
+'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-return [
+'allowed_methods' => ['*'],
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+'allowed_origins' => [
+    'http://localhost:3000',
+],
 
-    'allowed_methods' => ['*'],
+'allowed_origins_patterns' => [
+    '^https://blitzzquiz-[a-z0-9]+-testtrade404-7981s-projects\.vercel\.app$',
+],
 
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:3000'),
-        'http://localhost:3000',
-    ],
+'allowed_headers' => ['*'],
 
-    'allowed_origins_patterns' => [],
+'exposed_headers' => [],
 
-    'allowed_headers' => ['*'],
+'max_age' => 0,
 
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => true,
-];
+'supports_credentials' => true,
